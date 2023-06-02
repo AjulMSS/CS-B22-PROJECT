ed16c9dfb0674cefb75eab7722544abc33403e58
AjulMSS <133529012+AjulMSS@users.noreply.github.com>
2023-06-02 19:23:32 +0530

    Update b22project.py

--- b22project.py
+++ b22project.py
@@ -14,10 +14,67 @@ def calculate_angle(a,b,c):
     b = np.array(b) # mid landmark
     c = np.array(c) # end landmark
     
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = int(np.abs(radians*180.0/np.pi))
+    rad = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    ang = int(np.abs(rad*180.0/np.pi))
     
-    if angle >180.0:
-        angle = 360-angle
+    if ang >180.0:
+        ang = 360-ang
         
-    return angle 
+    return ang 
+with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read() #The image is read and obtained in BGR format
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False  
+      
+        results = pose.process(image)
+    
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        try:
+            landmarks = results.pose_landmarks.landmark
+            
+            shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+            
+            angle = calculate_angle(shoulder, elbow, wrist)
+            
+            cv2.putText(image, str(angle),tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            
+            if angle > 160:
+                stg = "down"
+            if angle < 30 and stg =='down':
+                stg="up"
+                cntr +=1
+                       
+        except:
+            pass
+        
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16),-1)
+
+        cv2.putText(image, 'REPS', (15,15), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(cntr), 
+                    (30,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)        
+        cv2.putText(image, 'STAGE', (95,15), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stg, 
+                    (80,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+        
+        
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                 )               
+        
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
